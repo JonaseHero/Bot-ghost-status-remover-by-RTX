666e0e705032aa9f370764865b22de122dbcc529
JonaseHero <161482682+JonaseHero@users.noreply.github.com>
2024-02-27 15:32:43 +0100

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = ["Homberg County RP","Bank of Homberg","Made by JonaseHero"];
 
 
 let currentIndex = 0;
